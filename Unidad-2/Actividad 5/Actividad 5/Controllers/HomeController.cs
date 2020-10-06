a7aff9547c313af225f7236f857c4ce187afa892
Narcizo Martinez <171g0236@rcarbonifera.tecnm.mx>
2020-10-05 21:33:51 -0500

    Tareas

--- Unidad-2/Actividad 5/Actividad 5/Controllers/HomeController.cs
+++ Unidad-2/Actividad 5/Actividad 5/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Actividad_5.Models;
+using Actividad_5.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Actividad_5.Controllers
 {
@@ -10,11 +13,17 @@ namespace Actividad_5.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            animalesContext context = new animalesContext();
+            var cl= context.Clase.OrderBy(x => x.Nombre).ToList();
+            return View(cl);
         }
-        public IActionResult Reptiles()
+        public IActionResult Clase()
         {
-            return View();
+           // ClaseViewModel vm = new ClaseViewModel();
+            animalesContext context = new animalesContext();
+            var clases = context.Especies.Include(x => x.Especie).Select(x=> new ClaseViewModel { NombreClase=x.Especie});
+
+            return View(clases);
         }
     }
 }
---
+++ Unidad-2/Actividad 5/Actividad 5/Models/ViewModels/ClaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Actividad_5.Models.ViewModels
+{
+    public class ClaseViewModel
+    {
+        public string NombreClase { get; set; }
+        public IEnumerable<Models.Clase> Clases { get; set; }
+    }
+}
