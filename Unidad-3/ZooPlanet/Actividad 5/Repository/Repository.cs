811c6612760a5f66a9ae12d0e7cb61e6d660a9bc
Narcizo Martinez <171g0236@rcarbonifera.tecnm.mx>
2020-11-01 21:01:29 -0600

    Tareas

---
+++ Unidad-3/ZooPlanet/Actividad 5/Repository/Repository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Actividad_5.Models;
+
+namespace Actividad_5.Repository
+{
+    public abstract class Repository<T> where T : class
+    {
+        public animalesContext context { get; set; }
+        public Repository(animalesContext c)
+        {
+            context = c;
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return context.Set<T>();
+        }
+        public virtual T GetById(object id)
+        {
+            return context.Find<T>(id);
+        }
+        public virtual void Insert(T entity)
+        {
+            if (Validate(entity))
+            {
+                context.Add(entity);
+                Save();
+            }
+        }
+        public virtual void Update(T entity)
+        {
+            if (Validate(entity))
+            {
+                context.Update(entity);
+                Save();
+            }
+        }
+        public virtual void Delete(T entity)
+        {
+            context.Remove(entity);
+            Save();
+        }
+        public virtual void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public virtual bool Validate(T entity)
+        {
+            return true;
+        }
+    }
+}
