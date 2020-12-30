9de7d9d87ecec2744a261982f8ec80e760af0cdf
Narcizo Martinez <171g0236@rcarbonifera.tecnm.mx>
2020-12-30 15:08:37 -0600

    Faltan Estilos

--- Unidad-4/ControlCuentasUsuario/ControlCuentasUsuario/Controllers/HomeController.cs
+++ Unidad-4/ControlCuentasUsuario/ControlCuentasUsuario/Controllers/HomeController.cs
@@ -18,12 +18,12 @@ namespace ControlCuentasUsuario.Controllers
 {
     public class HomeController : Controller
     {
-        public IWebHostEnvironment Enviroment { get; set; }
-        public HomeController(IWebHostEnvironment _enviroment)
+        public IWebHostEnvironment Environment { get; set; }
+        public HomeController(IWebHostEnvironment _environment)
         {
-            Enviroment = _enviroment;
+            Environment = _environment;
         }
-        [Authorize(Roles="User")]
+        [Authorize(Roles = "User")]
         public IActionResult Index()
         {
             return View();
@@ -40,9 +40,9 @@ namespace ControlCuentasUsuario.Controllers
             usuariosContext context = new usuariosContext();
             UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
             var datos = repos.GetByEmail(user.Email);
-            if (datos!=null &&HashHelper.GetHash(user.Password)==datos.Password)
+            if (datos != null && HashHelper.GetHash(user.Password) == datos.Password)
             {
-                if (datos.Active==1)
+                if (datos.Active == 1)
                 {
                     List<Claim> info = new List<Claim>();
                     info.Add(new Claim(ClaimTypes.Name, "Usuario" + datos.Username));
@@ -50,8 +50,8 @@ namespace ControlCuentasUsuario.Controllers
                     info.Add(new Claim("Email", datos.Email));
                     info.Add(new Claim("Username", datos.Username));
                     var claimIdentity = new ClaimsIdentity(info, CookieAuthenticationDefaults.AuthenticationScheme);
-                    var claimMain = new ClaimsPrincipal(claimMain);
-                    if (persistent==true)
+                    var claimMain = new ClaimsPrincipal(claimIdentity);
+                    if (persistent == true)
                     {
                         await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimMain, new AuthenticationProperties { IsPersistent = true });
                     }
@@ -69,8 +69,232 @@ namespace ControlCuentasUsuario.Controllers
             }
             else
             {
-                ModelState.AddModelError("","El correo o la contrasena son incorrectas")
+                ModelState.AddModelError("", "El correo o la contrasena son incorrectas");
+                return View(user);
             }
+        }
+        [Authorize(Roles = "User")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login");
+        }
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult register(Cuentum user, string pass, string pass2)
+        {
+            usuariosContext context = new usuariosContext();
+            UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
+            try
+            {
+                if (context.Cuenta.Any(x => x.Email == user.Email))
+                {
+                    ModelState.AddModelError("", "El correo ya se encuentra registrado");
+                    return View(user);
+                }
+                else
+                {
+                    if (pass == pass2)
+                    {
+                        user.Password = HashHelper.GetHash(pass);
+                        user.Token = CodeGeneratorHelper.GetCode();
+                        user.Active = 0;
+                        repos.Insert(user);
+                        MailMessage message = new MailMessage();
+                        message.From = new MailAddress("sistemascomputacionales7gmail.com", "Skyblock Dream");
+                        message.To.Add(user.Email);
+                        message.Subject = "Por favor confirma tu direccion de correo electronico en Skyblock Dream";
+                        string text = System.IO.File.ReadAllText(Environment.WebRootPath + "/ConfirmEmail.html");
+                        message.Body = text.Replace("{##codigo##}", user.Token.ToString());
+                        message.IsBodyHtml = true;
+                        SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+                        client.EnableSsl = true;
+                        client.UseDefaultCredentials = false;
+                        client.Credentials = new NetworkCredential("sistemascomputacionales7g.com", "sistemas7");
+                        client.Send(message);
+                        return RedirectToAction("Activate");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Las contrasenas no son iguales.");
+                        return View(user);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(user);
+            }
+        }
+        [AllowAnonymous]
+        public IActionResult Activate()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Activate(int code)
+        {
+            usuariosContext context = new usuariosContext();
+            UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
+            var us = context.Cuenta.FirstOrDefault(x => x.Token == code);
+            if (us != null && us.Active == 0)
+            {
+                var c = us.Token;
+                if (code == c)
+                {
+                    us.Active = 1;
+                    repos.Edit(us);
+                    return RedirectToAction("SignUp");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "El token ingresado no coincide.");
+                    return View((object)code);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "El usuario no existe.");
+                return View((object)code);
+            }
+        }
+        [Authorize(Roles = "User")]
+        public IActionResult ChangePass()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles = "User")]
+        public IActionResult ChangePass(string email, string pass, string newpass, string newpassconfirm)
+        {
+            usuariosContext context = new usuariosContext();
+            UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
+            try
+            {
+                var user = repos.GetByEmail(email);
+                if (user.Password != HashHelper.GetHash(pass))
+                {
+                    ModelState.AddModelError("", "La contrasena Ingresada es incorrecta.");
+                    return View();
+                }
+                else
+                {
+                    if (newpass!=newpassconfirm)
+                    {
+                        ModelState.AddModelError("", "Las nuevas contrasenas no coinciden.");
+                        return View();
+                    }
+                    else if (user.Password== HashHelper.GetHash(newpass))
+                    {
+                        ModelState.AddModelError("", "La nueva contrasena no puede ser igual a la anterior");
+                        return View();
+                    }
+                    else
+                    {
+                        user.Password = HashHelper.GetHash(newpass);
+                        repos.Edit(user);
+                        return RedirectToAction("SingUp");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View();
+            }
+        }
+        [AllowAnonymous]
+        public IActionResult RecoverPass()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult RecoverPass(string email)
+        {
+            try
+            {
+                usuariosContext context = new usuariosContext();
+                UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
+                var user = repos.GetByEmail(email);
+                if (user!=null)
+                {
+                    var temp = CodeGeneratorHelper.GetCode();
+                    MailMessage message = new MailMessage("sistemascomputacionales7@gmail.com", "Slyblock Dream");
+                    message.From = new MailAddress("sistemascomputacionales7g@gmail.com", "Skyblock Dream");
+                    message.To.Add(email);
+                    message.Subject = "Recupera tu contraseña de Skyblock Dream";
+                    string text = System.IO.File.ReadAllText(Environment.WebRootPath + "/Recover.html");
+                    message.Body = text.Replace("{##contraTemp##}", temp.ToString());
+                    message.IsBodyHtml = true;
+
+                    SmtpClient cliente = new SmtpClient("smtp.gmail.com", 587);
+                    cliente.EnableSsl = true;
+                    cliente.UseDefaultCredentials = false;
+                    cliente.Credentials = new NetworkCredential("sistemascomputacionales7g@gmail.com", "sistemas7g");
+                    cliente.Send(message);
+                    user.Password = HashHelper.GetHash(temp.ToString());
+                    repos.Edit(user);
+                    return RedirectToAction("SignUp");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "El correo que ingreso no se encuentra registrado :(");
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View((object)email);
+            }
+        }
+        [Authorize(Roles ="User")]
+        public IActionResult DeleteAccount()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult DeleteAccount(string email, string password)
+        {
+            try
+            {
+                usuariosContext context = new usuariosContext();
+                UserRepository<Cuentum> repos = new UserRepository<Cuentum>(context);
+                var user = repos.GetByEmail(email);
+                if (user!=null)
+                {
+                    if (HashHelper.GetHash(password)==user.Password)
+                    {
+                        repos.Delete(user);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "La contrasena esta incorrecta");
+                        return View();
+                    }
+                }
+                return RedirectToAction("SignUp");
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Ha ocurrido un error, por favor intentelo de nuevo en un par de minutos");
+                return View();
+            }
+        }
+
+        [AllowAnonymous]
+        public IActionResult Denied()
+        {
+            return View();
         }
     }
 }
--- Unidad-4/ControlCuentasUsuario/ControlCuentasUsuario/Models/usuariosContext.cs
+++ Unidad-4/ControlCuentasUsuario/ControlCuentasUsuario/Models/usuariosContext.cs
@@ -23,7 +23,6 @@ namespace ControlCuentasUsuario.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=usuarios", x => x.ServerVersion("5.7.18-mysql"));
             }
         }
@@ -39,7 +38,7 @@ namespace ControlCuentasUsuario.Models
 
                 entity.Property(e => e.IdUser).HasColumnType("int(11)");
 
-                entity.Property(e => e.Activo)
+                entity.Property(e => e.Active)
                     .HasColumnType("bit(1)")
                     .HasDefaultValueSql("b'0'");
 
