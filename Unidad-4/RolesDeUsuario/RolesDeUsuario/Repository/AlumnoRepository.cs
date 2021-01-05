d57949a515972f6e405537c303798d2a41b5afb2
Narcizo Martinez <171g0236@rcarbonifera.tecnm.mx>
2021-01-04 23:27:00 -0600

    Terminado

---
+++ Unidad-4/RolesDeUsuario/RolesDeUsuario/Repository/AlumnoRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RolesDeUsuario.Models;
+
+namespace RolesDeUsuario.Repository
+{
+    public class AlumnoRepository:RolUsuarioRepository<Alumno>
+    {
+        public AlumnoRepository(rolesusuarioContext _context):base(_context)
+        {
+
+        }
+        public Alumno GetByNControl(string ncontrol)
+        {
+            return Context.Alumno.FirstOrDefault(x => x.Ncontrol == ncontrol);
+        }
+        public override bool Validate(Alumno entity)
+        {
+            if (string.IsNullOrEmpty(entity.Ncontrol))
+                throw new Exception("Por favor asigne un numero de control.");
+            if (string.IsNullOrEmpty(entity.Nombre))
+                throw new Exception("Por favor asigne un nombre.");
+            if (entity.IdMaestro <= 0 || entity.IdMaestro == null)
+                throw new Exception("Debe asignar el alumno a un Maestro.");
+            return true;
+        }
+    }
+}
---
+++ Unidad-4/RolesDeUsuario/RolesDeUsuario/Repository/RolUsuarioRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RolesDeUsuario.Models;
+
+namespace RolesDeUsuario.Repository
+{
+    public class RolUsuarioRepository<T> where T:class
+    {
+        public rolesusuarioContext Context { get; set; }
+        public RolUsuarioRepository(rolesusuarioContext _context)
+        {
+            Context = _context;
+        }
+        public virtual IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>();
+        }
+        public T GetTById(object id)
+        {
+            return Context.Find<T>(id);
+        }
+        public virtual void Insert(T entity)
+        {
+            if (Validate(entity))
+            {
+                Context.Update<T>(entity);
+                Context.SaveChanges();
+            }
+        }
+        public void Delete(T entity)
+        {
+            Context.Remove<T>(entity);
+            Context.SaveChanges();
+        }
+        public virtual void Update(T entity)
+        {
+            if (Validate(entity))
+            {
+                Context.Update<T>(entity);
+                Context.SaveChanges();
+            }
+        }
+        public virtual bool Validate(T entity)
+        {
+            return true;
+        }
+    }
+}
