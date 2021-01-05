54a13e6a7ae512d031762638c9ab593f36bd17ac
Narcizo Martinez <171g0236@rcarbonifera.tecnm.mx>
2021-01-04 23:40:56 -0600

    Terminado

--- Unidad-4/RolesDeUsuario/RolesDeUsuario/Repository/AlumnoRepository.cs
+++ Unidad-4/RolesDeUsuario/RolesDeUsuario/Repository/AlumnoRepository.cs
@@ -12,18 +12,22 @@ namespace RolesDeUsuario.Repository
         {
 
         }
-        public Alumno GetByNControl(string ncontrol)
+        public Alumno GetByNControl(int ncontrol)
         {
             return Context.Alumno.FirstOrDefault(x => x.Ncontrol == ncontrol);
         }
         public override bool Validate(Alumno entity)
         {
-            if (string.IsNullOrEmpty(entity.Ncontrol))
+            if (string.IsNullOrEmpty(entity.Ncontrol.ToString()))
                 throw new Exception("Por favor asigne un numero de control.");
             if (string.IsNullOrEmpty(entity.Nombre))
                 throw new Exception("Por favor asigne un nombre.");
             if (entity.IdMaestro <= 0 || entity.IdMaestro == null)
                 throw new Exception("Debe asignar el alumno a un Maestro.");
+            if (entity.Ncontrol.ToString().Length <= 0)
+                throw new Exception("El numero de control debe ser mayor a 0.");
+            if (entity.Ncontrol.ToString().Length>11)
+                throw new Exception("El numero de control no puede sobrepasar 11 digitos");
             return true;
         }
     }
